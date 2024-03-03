7c20badc4bb9148455d85308ac1eeee8b8039739
brajithdr <brajithdr@gmail.com>
2024-03-03 15:23:37 +0530

    PDF Trainer

---
+++ Input_Parameters.py
@@ -0,0 +1,15 @@
+
+
+
+llm_temperature_dict = {
+    "Chat bot":0.5,
+    "Email Parser":0.3,
+    'Summarise Text':0.5,
+    'Code Generation':0.8,
+    'Code Refactoring':0.2,
+    "Casual Conversation": 0.7,
+    "Formal Writing": 0.5,
+    "Creative Writing": 0.8,
+    "Technical Documentation": 0.3,
+    "Humorous Content": 0.9
+}
